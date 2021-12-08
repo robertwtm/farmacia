8b037d33940aecbb81e17d6ec4602e3e6bc1b91f
Augusto Cardoso Xavier <augustocx15@gmail.com>
2021-12-08 10:19:50 -0300

    Subindo os arquivos

---
+++ src/main/java/org/generation/farmacia/controller/CategoriaController.java
@@ -0,0 +1,59 @@
+package org.generation.farmacia.controller;
+
+import java.util.List;
+
+import org.generation.farmacia.model.Categoria;
+import org.generation.farmacia.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin("*")
+public class CategoriaController {
+	
+	@Autowired
+	private CategoriaRepository repository; 
+	
+	@GetMapping("/categoria") 
+	public ResponseEntity<List<Categoria>> findAllCategoria() {		
+		return ResponseEntity.ok(repository.findAll ());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> findByIdCategoria(@PathVariable Long id) {
+		return repository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+					.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/descricao/{descricao}")
+	public ResponseEntity<List<Categoria>> findByDescricaoCategoria(@PathVariable String descricao) {
+		return ResponseEntity.ok(repository.FindAllByDescricaoCategoriaContainingIgnoreCase(descricao));
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Categoria> postCategoria(@RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Categoria> putCategoria(@RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(categoria));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteCategoria(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
+}
---
+++ src/main/java/org/generation/farmacia/model/Categoria.java
@@ -0,0 +1,58 @@
+package org.generation.farmacia.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@NotBlank
+	@Size(min = 5, max = 25)
+	private String nome;
+	
+	@NotNull
+	@NotBlank
+	@Size(min = 5, max = 255)
+	private String descricaoCategoria;
+	
+	//private List<Produto> produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricaoCategoria() {
+		return descricaoCategoria;
+	}
+
+	public void setDescricaoCategoria(String descricaoCategoria) {
+		this.descricaoCategoria = descricaoCategoria;
+	}
+	
+}
