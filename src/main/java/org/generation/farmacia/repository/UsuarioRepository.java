308e6ce36a0771f1099905e789451157cab318b4
Robert Willian Tavares Mendes <31051742+robertwtm@users.noreply.github.com>
2021-12-17 19:28:24 -0300

    Create UsuarioRepository.java

---
+++ src/main/java/org/generation/farmacia/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package org.generation.farmacia.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.generation.farmacia.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	public Optional<Usuario> findByUsuario(String usuario);
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+}
