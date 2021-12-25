1bf52f2eeabf50a8fcbf36f3b74a4b6d6107c002
giovana borges <gigi12345as17@gmail.com>
2021-12-24 22:33:36 -0300

    @GiovanaBorges add dependecy Swagger

--- src/main/java/org/generation/farmacia/service/UsuarioService.java
+++ src/main/java/org/generation/farmacia/service/UsuarioService.java
@@ -1,6 +1,7 @@
 package org.generation.farmacia.service;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 import org.apache.commons.codec.binary.Base64;
@@ -8,8 +9,10 @@ import org.generation.farmacia.model.UserLogin;
 import org.generation.farmacia.model.Usuario;
 import org.generation.farmacia.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UsuarioService {
@@ -18,25 +21,24 @@ public class UsuarioService {
 	private UsuarioRepository repository;
 
 	public Usuario CadastrarUsuario(Usuario usuario) {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		if(repository.findByUsuario(usuario.getUsuario()).isPresent()){
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Usuário em uso");
+		}
 
-		String senhaEncoder = encoder.encode(usuario.getSenha());
+		String senhaEncoder = encodePass(usuario.getSenha());
+		String name = usuario.getNome();
 		usuario.setSenha(senhaEncoder);
+		usuario.setNome(name);
 
 		return repository.save(usuario);
 	}
 
 	public Optional<UserLogin> Logar(Optional<UserLogin> user) {
-
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
-
 		if (usuario.isPresent()) {
-			if (encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
+			if (comparePass(user.get().getSenha(), usuario.get().getSenha())) {
 
-				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
-				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
-				String authHeader = "Basic " + new String(encodedAuth);
+				String authHeader = generateBasicToken(user.get().getUsuario() ,user.get().getSenha() );
 
 				user.get().setToken(authHeader);
 				user.get().setNome(usuario.get().getNome());
@@ -47,5 +49,23 @@ public class UsuarioService {
 			}
 		}
 		return null;
+	}
+
+	private String encodePass(String passWord){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		String passEncode = encoder.encode(passWord);
+		return passEncode;
+	}
+
+	private String generateBasicToken(String email, String passWord){
+		String structure = email + ":" + passWord;
+		byte[] Base64structure = Base64.encodeBase64(structure.getBytes(Charset.forName("US-ASCII")));
+		return "Basic" + new String(Base64structure);
+	}
+
+	private boolean comparePass(String newPass, String savePass){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder.matches(newPass,savePass);
 	}
 }
